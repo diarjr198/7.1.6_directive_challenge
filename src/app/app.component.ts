7d8e0df96eb985a302c74edbcdc3e0a840dad934
diarjr <diar.student@gmail.com>
2021-11-26 22:42:41 +0700

    third challenges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'directive-challenge';
+  isShow = false;
+  number: number[] = [];
+
+  change(): any {
+    // this.number++;
+    if (this.isShow === true) {
+      this.isShow = false;
+    } else {
+      this.isShow = true;
+    }
+    this.number.push(this.number.length + 1);
+    console.log(this.number.length);
+  }
 }
